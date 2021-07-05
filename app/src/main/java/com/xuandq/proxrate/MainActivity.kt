bbe5d7c884edcee77513bfeebb26f1abc384caac
Xuân Đỗ Quý <xuandq2601@gmail.com>
2021-07-05 14:41:45 +0700

    Initial commit

---
+++ app/src/main/java/com/xuandq/proxrate/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.xuandq.proxrate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xuandq.rate.ProxRateDialog
+import com.xuandq.rate.RatingDialogListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val config = ProxRateDialog.Config()
+        config.setListener(object : RatingDialogListener{
+            override fun onChangeStar(rate: Int) {
+
+            }
+
+            override fun onSubmitButtonClicked(rate: Int, comment: String?) {
+
+            }
+
+            override fun onLaterButtonClicked() {
+
+            }
+        })
+
+        ProxRateDialog.init(this, config)
+
+        txt.setOnClickListener {
+            ProxRateDialog.showIfNeed(supportFragmentManager)
+        }
+    }
+}
